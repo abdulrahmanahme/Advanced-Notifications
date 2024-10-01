119aec564ef457df77680f7582512bd0403782d4
abdulrahmanahme <abdelramanahmed00@gmail.com>
2024-10-01 20:41:41 +0300

    - update notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:notifications/home_screen/home_screen.dart';
 import 'package:notifications/notification/notification.dart';
+var navigatorKey =GlobalKey<NavigatorState>();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await NotificationHelper.init();
-  
+
   runApp(const MyApp());
 }
 
@@ -19,6 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      navigatorKey: navigatorKey,
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
--- lib/notification/notification.dart
+++ lib/notification/notification.dart
@@ -2,21 +2,50 @@ import 'dart:developer';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter/material.dart';
+import 'package:notifications/main.dart';
 
 class NotificationHelper {
   static String? firebaseUserToken;
   static init() async {
     await Firebase.initializeApp();
     await permissionNotifications();
+
     /// Get Firebase User Token
     firebaseUserToken = await FirebaseMessaging.instance.getToken();
+    log('dd  ${firebaseUserToken}');
 
     /// Called when app in foreground
-    FirebaseMessaging.onMessage.listen((RemoteMessage remoteMessage) {});
+    FirebaseMessaging.onMessage.listen((RemoteMessage remoteMessage) {
+      log('Received a message in the foreground: ${remoteMessage.notification?.title}');
+      final snackBar = SnackBar(
+        content: Text('${remoteMessage.notification?.title}'),
+        action: SnackBarAction(
+          label: '${remoteMessage.notification?.body}',
+          onPressed: () {
+            print('Undo action pressed');
+          },
+        ),
+      );
+      ScaffoldMessenger.of(navigatorKey.currentContext!).showSnackBar(snackBar);
+    });
 
     /// Called when app in background
-    FirebaseMessaging.onMessageOpenedApp.listen(_handleMessage);
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage remoteMessage) {
+      log('Received a message in the background: ${remoteMessage.notification?.title}');
+      log('Received a message in the body: ${remoteMessage.notification?.body}');
+      final snackBar = SnackBar(
+        content: Text('${remoteMessage.notification?.title}'),
+        action: SnackBarAction(
+          label: '${remoteMessage.notification?.body}',
+          onPressed: () {
+            // Code to execute when "Undo" is pressed
+            print('Undo action pressed');
+          },
+        ),
+      );
+      ScaffoldMessenger.of(navigatorKey.currentContext!).showSnackBar(snackBar);
+    });
 
     /// Called when the app is terminated
     RemoteMessage? initialMessage =
@@ -27,32 +56,82 @@ class NotificationHelper {
     }
   }
 
-  static void _handleMessage(RemoteMessage remoteMessage) {}
-
-  /// Local Notification in Foreground
-
-  AndroidNotificationChannel channel = const AndroidNotificationChannel(
-    'high_importance_channel', // id
-    'High Importance Notifications', // title
-    description:
-        'This channel is used for important notifications.', // description
-    importance: Importance.max,
-  );
-
-  void handleLocalNotificationInForeground() async {
-    final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-        FlutterLocalNotificationsPlugin();
-
-    await flutterLocalNotificationsPlugin
-        .resolvePlatformSpecificImplementation<
-            AndroidFlutterLocalNotificationsPlugin>()
-        ?.createNotificationChannel(channel);
-
-
-
-
+  static void _handleMessage(RemoteMessage remoteMessage) {
+    log('Received a message in the background: ${remoteMessage.notification?.title}');
+    log('Received a message in the body: ${remoteMessage.notification?.body}');
+    final snackBar = SnackBar(
+        content: Text('${remoteMessage.notification?.title}'),
+        action: SnackBarAction(
+          label: '${remoteMessage.notification?.body}',
+          onPressed: () {
+            // Code to execute when "Undo" is pressed
+            print('Undo action pressed');
+          },
+        ),
+      );
+      ScaffoldMessenger.of(navigatorKey.currentContext!).showSnackBar(snackBar);
   }
 
+  // /// Local Notification in Foreground
+
+  // AndroidNotificationChannel channel = const AndroidNotificationChannel(
+  //   'high_importance_channel', // id
+  //   'High Importance Notifications', // title
+  //   description:
+  //       'This channel is used for important notifications.',
+  //       // description
+
+  //   importance: Importance.max,
+  // );
+
+  // void handleLocalNotificationInForeground(RemoteMessage message) async {
+  //   final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+  //       FlutterLocalNotificationsPlugin();
+
+  //   await flutterLocalNotificationsPlugin
+  //       .resolvePlatformSpecificImplementation<
+  //           AndroidFlutterLocalNotificationsPlugin>()
+  //       ?.createNotificationChannel(channel);
+
+  //   RemoteNotification? notification = message.notification;
+  //   AndroidNotification? android = message.notification?.android;
+
+  //   if (notification != null && android != null) {
+  //     flutterLocalNotificationsPlugin.show(
+  //       notification.hashCode,
+  //       notification.title,
+  //       notification.body,
+  //       NotificationDetails(
+  //         android: AndroidNotificationDetails(
+  //           channel.id,
+  //           channel.name,
+  //           channelDescription: channel.description,
+  //           icon: android.smallIcon,
+  //   //         styleInformation:  BigPictureStyleInformation(
+  //   //   bigPicture,
+  //   //   largeIcon: largeIcon,
+  //   // )
+  //           // other properties...
+  //         ),
+  //       ),
+  //     );
+  //   }
+  // }
+
+  // Future<Uint8List> _getByteArrayFromUrl(String url) async {
+  //   final http.Response response = await http.get(Uri.parse(url));
+  //   return response.bodyBytes;
+  // }
+
+  // final ByteArrayAndroidBitmap largeIcon = ByteArrayAndroidBitmap(
+  //     await _getByteArrayFromUrl(notification.android!.imageUrl!));
+  // final ByteArrayAndroidBitmap bigPicture = ByteArrayAndroidBitmap(
+  //     await _getByteArrayFromUrl(notification.android!.imageUrl!));
+  // final BigPictureStyleInformation bigPictureStyleInformation =
+  //     BigPictureStyleInformation(
+  //   bigPicture,
+  //   largeIcon: largeIcon,
+  // );
   /// Notification permission
   static Future permissionNotifications() async {
     FirebaseMessaging messaging = FirebaseMessaging.instance;
@@ -76,5 +155,4 @@ class NotificationHelper {
       log('User declined or has not accepted permission');
     }
   }
-
 }
